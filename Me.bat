8f4d11b60c1b9464c38f9cf6826c1d97dae69434
WatchyerbakModeratot <117971094+WatchyerbakModeratot@users.noreply.github.com>
2022-11-11 09:54:06 -0600

    Create Me.bat
    Run

---
+++ Me.bat
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @WatchyerbakModeratot
+- 👀 I’m interested in Microsoft.
+- 🌱 I’m currently learning Technology.
+- 💞️ I’m looking to collaborate on Microsoft Studio 2+ for Business.
+- 📫 How to reach me is calling +15043558888.
+
+<!---
+Sir Baron von Count Benjamin Louis Wilkinson is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
